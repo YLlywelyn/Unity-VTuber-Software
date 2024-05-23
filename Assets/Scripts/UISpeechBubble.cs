a6c51fdb2eb2b889e306f2128e3dda76de025676
Ylva Llywelyn <ylva.the.defender@gmail.com>
2024-05-23 18:59:27 +0100

    Started on speech bubble class

---
+++ Assets/Scripts/UISpeechBubble.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using TwitchChatConnect.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISpeechBubble : MonoBehaviour
+{
+    [Min(0f)]
+    public float destroyAfterSeconds = 30f;
+    public TwitchChatMessage message;
+
+    public TextMeshProUGUI messageText;
+
+    public RectTransform rectTransform { get { return (RectTransform)transform; } }
+
+    private void Start()
+    {
+        messageText.text = message.Message;
+
+        if (destroyAfterSeconds > 0f)
+            StartCoroutine(DestroyTimer());
+    }
+
+    private void Update()
+    {
+        rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.x, ((messageText.textInfo.lineCount + 1) * 20) + 30);
+        Debug.Log("line count: " + messageText.textInfo.lineCount);
+    }
+
+    IEnumerator DestroyTimer()
+    {
+        yield return new WaitForSeconds(destroyAfterSeconds);
+        Destroy(gameObject);
+    }
+}

e6190ed5f8b79dfcd3c9b19cf82b8e9e396226a4
Ylva Llywelyn <ylva.the.defender@gmail.com>
2024-06-14 00:29:54 +0100

    Implemented Transition Manager.cs as a singleton that handles scene switching with a fade effect

---
+++ Assets/Scripts/Transition Manager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionManager : MonoBehaviour
+{
+    public static TransitionManager instance { get; private set; }
+    public bool transitioning { get; private set; }
+
+    [Min(0f)]
+    public float fadeSeconds = 5f;
+
+    public Material fullScreenMaterial;
+
+    const string propertyName = "_Cutoff";
+    float cutoff
+    {
+        get { return fullScreenMaterial.GetFloat(propertyName); }
+        set { fullScreenMaterial.SetFloat(propertyName, value); }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        FadeIn();
+    }
+
+    public void FadeIn() => StartCoroutine(Transition(1, 0));
+    public void FadeOut() => StartCoroutine(Transition(0, 1));
+    IEnumerator Transition(float a, float b)
+    {
+        transitioning = true;
+        cutoff = a;
+        for (float t = 0f; cutoff != b; t += Time.deltaTime/fadeSeconds)
+        {
+            cutoff = Mathf.Clamp01(Mathf.SmoothStep(a, b, t));
+            yield return null;
+        }
+        transitioning = false;
+    }
+
+    IEnumerator TransitionToScene(int index)
+    {
+        yield return new WaitUntil(() => !transitioning);
+        FadeOut();
+        yield return new WaitUntil(() => !transitioning);
+
+        var op = SceneManager.LoadSceneAsync(index);
+        while (!op.isDone)
+            yield return null;
+
+        FadeIn();
+        yield return new WaitUntil(() => !transitioning);
+    }
+}
